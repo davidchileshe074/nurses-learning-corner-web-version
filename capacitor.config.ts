e256bdaf1a883e2f3c2cde2ca0dc6159358a6e7b
DavidChileshe <davidchileshe33@gmail.com>
2026-02-19 23:46:34 +0200

    remove  capacior

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nursecornere.app',
-  appName: 'Nurse Corner',
-  webDir: 'out'
-};
-
-export default config;
